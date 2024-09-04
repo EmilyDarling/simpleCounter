1c8f18abd5dcbf594071ad0978cd90b19b1d6c22
Angelina <angelaaquin13@gmail.com>
2024-09-04 21:59:09 +0000

    completed

--- src/js/component/cardBase.jsx
+++ src/js/component/cardBase.jsx
@@ -6,10 +6,9 @@ const CardBase = (props) => {
 	return (
                        
             <div className="bg-black rounded-3 border border-3 border-secondary mt-1" style={{width:150, height:190}}>
-                <p className= "text-center mb-3" style={{fontSize:150}}>{props.number}</p>
-                <span>{props.icon}</span>
+                {props.number ? <p className= "text-center card-inner text-white" style={{fontSize:150, height:170}}>{props.number}</p> :
+                <i className="far fa-clock card-inner text-white" style={{fontSize:120, marginTop: 35}}></i> }
             </div>
-            
         
     );
 };
--- src/js/component/imgCard.jsx
+++ src/js/component/imgCard.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import CardBase from "./cardBase.jsx";
 
-const icon = '<i class="far fa-clock text-light d-flex justify-content-center mt-4" style={{fontSize:100}}></i>' ;
+
 const ImgCard = () => {
 	return (
-        <CardBase icon='<i class="far fa-clock text-light d-flex justify-content-center mt-4" style={{fontSize:100}}></i>' / >
+        <><CardBase clock= {'<i class="far fa-clock"></i>'} / ></>
 	);
 };
 
